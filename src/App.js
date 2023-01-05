4d2102f2382a387c11ae0176ddfa5d10378c3adf
Sharon Sofer <sharon.sofer@gmail.com>
2023-01-05 21:32:34 +0200

    Getting better images

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div>
       <Navbar />
+
       <Card />
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,21 +3,27 @@ import classes from "./Card.module.css";
 export default function Card() {
   return (
     <div className={classes.card}>
-      <img
-        src="../../images/Kattie.PNG"
-        alt=""
-        className={classes.card_image}
-      />
-      <div className={classes.card_stats}>
-        <img src="../../images/star.png" alt="" className={classes.card_star} />
-        <span>5.0</span>
-        <span className={classes.gray}>(6) • </span>
-        <span className={classes.gray}>USA</span>
-      </div>
-      <p>Life Lessons with Katie Zaferes</p>
-      <p>
-        <span className={classes.bold}>From $136</span> / person
-      </p>
+      <section>
+        <img
+          src="../../images/Kattie.PNG"
+          alt=""
+          className={classes.card_image}
+        />
+        <div className={classes.card_stats}>
+          <img
+            src="../../images/star.png"
+            alt=""
+            className={classes.card_star}
+          />
+          <span>5.0</span>
+          <span className={classes.gray}>(6) • </span>
+          <span className={classes.gray}>USA</span>
+        </div>
+        <p>Life Lessons with Katie Zaferes</p>
+        <p>
+          <span className={classes.bold}>From $136</span> / person
+        </p>
+      </section>
     </div>
   );
 }
